0cb32dea212d5ecc03eb2ee7b4d524b48657adeb
Caoilte Purcell <117462706@umail.ucc.ie>
2018-03-08 11:41:17 +0000

    stylign e bus 3

--- EBusiness/Ebus3.php
+++ EBusiness/Ebus3.php
@@ -5,9 +5,49 @@ session_start();
 <html>
     <head>
         <title>RECEIPT</title>
+        
+        <!--Adding the Navigation bar layout and Background-->
+        <style>
+            ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+            }
+            li {
+            float: left;
+            }
+            li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            }
+            li a:hover {
+            background-color: #111;
+            }
+            body{
+            text-align: center;
+            background:url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB0A3ElWxecNYPZvMbbf_TOltUB7eapDWzK9t0L5cI4DETO9phhw");
+            background-repeat: no-repeat;
+            background-size: cover;
+            }
+        </style>
     </head>
     <body>
         <h4>RECEIPT</h4>
+        
+        <!--Populating the Navigation bar with functional buttons--> 
+        <ul>
+        <li><a href="Homepage.html">Homepage</a></li>
+        <li><a class="active" href="CV/cv_page1.html">Curriculum Vitae</a></li>
+        <li><a href="/Interests/Sport.html">Interests</a></li>
+        <li><a href="EBusiness/E_Business1.php">E-Business</a></li>
+        <li><a href="https://dashboard.heroku.com/apps/is1113-117462706">Heroku</a></li>
+        <li><a href="https://github.com/CaoiltePurcell/is1113117462706/graphs/commit-activity">GitHub</a></li>
+        </ul>
                 
         <?php
         // Echo session variables that were set on previous page
